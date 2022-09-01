d8d451b58c4f254f67dc62025fd6f081ec318d77
Md. Mahadi Hassan Riyadh <mahadi_hassan3931@yahoo.com>
2022-09-02 01:19:59 +0600

    ...

--- src/redux/filters/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const STATUSCHANGED = "filters/statusChanged"; // if the user wanna see all the tasks or all the completed / incompleted tasks
-export const COLORCHANGED = "filters/colorChanged"; // if the use wanna see all the tasks or all the red/green/yellow colored tasks
