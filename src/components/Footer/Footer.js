581b1d56415378ea7c990bff21c430be06c65b50
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-03 20:22:58 +0600

    done with react router setup

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -6,13 +7,13 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex justify-between gap-2 border-t text-sm text-slate-400">
         <div>Copyright 2022 Learn with Sumit.</div>
         <div>
-          <a
-            href="https://youtube.com/learnwithsumit"
+          <Link
+            to="https://youtube.com/learnwithsumit"
             target="_blank"
             rel="noreferrer"
           >
             YouTube Channel
-          </a>
+          </Link>
         </div>
       </div>
     </section>
