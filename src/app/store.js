3897f34f12b677d90a5391af0bc2a4867d1e1a8c
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-04 23:39:04 +0600

    new project initiated

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import videosReducer from "../features/videos/videosSlice";
-import tagsReducer from "../features/tags/tagsSlice";
-import videoReducer from "../features/video/videoSlice";
-import relatedVideosReducer from "../features/relatedVideos/relatedVideosSlice";
-import filterReducer from "../features/filter/filterSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    videos: videosReducer,
-    tags: tagsReducer,
-    video: videoReducer,
-    relatedVideos: relatedVideosReducer,
-    filter: filterReducer,
+    counter: counterReducer,
   },
 });
--- src/features/tags/tagsAPI.js
+++
@@ -1,7 +0,0 @@
-import axiosInstance from "../../utilities/axios";
-
-export const getTags = async () => {
-  const response = await axiosInstance.get("/tags");
-
-  return response.data;
-};
