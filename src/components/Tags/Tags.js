f68dcfacb92593557046f8bf41789f304ec5bc4f
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-04 12:42:19 +0600

    tag feature done

--- src/components/Tags/Tags.js
+++ src/components/Tags/Tags.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTagsAsync } from '../../features/tags/tagsSlice';
+import Loading from '../UI/Loading';
 import Tag from './Tag';
 
 const Tags = () => {
-  return (
+  const dispatch = useDispatch();
+  const { tags, isLoading, isError, error } = useSelector((state) => state.tags);
+  useEffect(() => {
+    dispatch(fetchTagsAsync());
+  }, [dispatch])
+
+  return tags?.length > 0 ? (
     <section>
       <div
         className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto"
       >
-        <Tag></Tag>
+        {tags.map((tag) => <Tag key={tag.id} tag={tag} />)}
       </div>
     </section>
-  );
+  ) : null;
 };
 
 export default Tags;
