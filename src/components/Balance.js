898f97e272ca41ad08e9e178f9e18717e5414f62
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-06 18:53:58 +0600

    done with module 7 and expense tracker project

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import numberWithCommas from "../utils/numberWithCommas";
 
 const Balance = () => {
+  const { transactions } = useSelector((state) => state.transaction);
+  const amounts = transactions?.reduce((total, transaction) => {
+    if (transaction.type === "income") {
+      return total + transaction.amount;
+    } else {
+      return total - transaction.amount;
+    }
+
+  }, 0);
   return (
     <div className="top_card">
       <p>Your Current Balance</p>
       <h3>
-        <span>৳</span>
-        <span>10500</span>
+        <span>৳ </span>
+        <span>{ transactions?.length > 0 ? numberWithCommas(amounts) : 0}</span>
       </h3>
     </div>
   );
--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -3,6 +3,7 @@ import editImage from "../../assets/images/edit.svg";
 import deleteImage from "../../assets/images/delete.svg";
 import { useDispatch } from "react-redux";
 import { editActive, removeTransaction } from "../../features/transaction/transactionSlice";
+import numberWithCommas from "../../utils/numberWithCommas";
 
 const Transaction = ({ transaction }) => {
   const { id, type, amount, name } = transaction || {};
@@ -20,7 +21,7 @@ const Transaction = ({ transaction }) => {
     <li className={`transaction ${type} income`}>
       <p>{name}</p>
       <div className="right">
-        <p>৳ {amount}</p>
+        <p>৳ {numberWithCommas(amount)}</p>
         <button className="link" onClick={handleEdit}>
           <img className="icon" src={editImage} alt="edit" />
         </button>
