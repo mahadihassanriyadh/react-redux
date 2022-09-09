18e9417095de23a19da5a0187b1ff4ed0f18d8bf
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-10 00:52:17 +0600

    all videos loading with rtk query

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,11 +1,30 @@
 import { useGetVideosQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
-    const { data: videos, isLoading, isError } = useGetVideosQuery();
+    const { data: videos, isLoading, isError, error } = useGetVideosQuery();
+
+    // decide what to render
+    let content = null;
+    if (isLoading) {
+        content = (
+            <>
+                <VideoLoader /> <VideoLoader /> <VideoLoader /> <VideoLoader />
+            </>
+        );
+    } else if (isError) {
+        content = <Error error={error} />;
+    } else if (videos?.length === 0) {
+        content = <Error error={"No videos found!"} />
+    } else { 
+        content = videos.map((video) => <Video key={video.id} video={video} />);
+    }
+
     return (
         <>
-            <Video />
+            {content}
         </>
     );
 }
