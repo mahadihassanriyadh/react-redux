82c3fac9d0faa449a3e7d466b9d8cb818850d304
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-09 20:56:05 +0600

    ....

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,14 +1,10 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
 import Video from "./Video";
 
 export default function Videos() {
+    const { data: videos, isLoading, isError } = useGetVideosQuery();
     return (
         <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
             <Video />
         </>
     );
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -16,8 +16,11 @@ export const apiSlice = createApi({
 
         // এখানে getVideos একটা query, এবং আমরা যে এই query করতে চাচ্ছি এখানে, তার jonyjony
         getVideos: builder.query({
-            // we could have return a direct string too. but we are using a function here to make it dynamic. this function will return a string.
+            // we could have return a direct string too. but we are using a function here to make it dynamic. this function will return a string. We can recieved one prameter as well, if we need to pass multiple parameters then we can use an object.
             query: () => `videos`,
         }),
     })
 });
+
+// so how do we get the queries, redux query will return us hooks. so we can use the hooks in our components. Here in our endpoints we declared a property with the name getVideos, that is why apiSlice returned us with a hook with the same name by adding a suffix and prefix. use + GetVideos + Query.
+export const { useGetVideosQuery } = apiSlice;
