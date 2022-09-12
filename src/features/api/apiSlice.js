c4c70cd572e84ef268ccbe10c9766aae95d7c0c5
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-12 20:55:24 +0600

    rtk query is awesome and so beautiful

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -9,31 +9,39 @@ export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: "http://localhost:9000/",
     }),
-    tagTypes: ["Videos"],
+    // whitelisting the tags
+    tagTypes: ["Videos", "Video", "RelatedVideos"],
     // endpoints is an object where we can define all the endpoints we want to use in our application. such as get, post, put, delete, etc. Which is similar to the builder property in createSlice().
-    //  
+    //
     endpoints: (builder) => ({
         // getvideos আমাদের object এর প্রথম property। getVideos হচ্ছে ultimately আমাদের API এর নাম। যেহেতু এটা rtk query সে সবকিছু query এর মত চিন্তা করে। আমরা server থেকে যে data আনি সেটাই query। আরকটা জিনিস এখানে আছে, তা হলো mutation। mutation মানেই পরিবর্তন করা। Server থেকে data আনাই query, আর server এ post, put, pach, delete, যা server এর data পরিবর্তন করে দিবে তা ই mutation.
 
         // এখানে getVideos একটা query, এবং আমরা যে এই query করতে চাচ্ছি এখানে, তার jonyjony
-    getVideos: builder.query({
-        // we could have return a direct string too. but we are using a function here to make it dynamic. this function will return a string. We can recieved one prameter as well, if we need to pass multiple parameters then we can use an object.
-        query: () => `videos`,
-        // keepUnusedDataFor: 60, means if we have already fetched the data, then we will not fetch it again for 60 seconds. this is optional. By default it is set to 60 means it will fetcg unused data every 60 seconds. 
-        keepUnusedDataFor: 120,
-        providesTags: ["Videos"],
-    }),
+        getVideos: builder.query({
+            // we could have return a direct string too. but we are using a function here to make it dynamic. this function will return a string. We can recieved one prameter as well, if we need to pass multiple parameters then we can use an object.
+            query: () => `videos`,
+            // keepUnusedDataFor: 60, means if we have already fetched the data, then we will not fetch it again for 60 seconds. this is optional. By default it is set to 60 means it will fetcg unused data every 60 seconds.
+            keepUnusedDataFor: 120,
+            providesTags: ["Videos"],
+        }),
         getVideo: builder.query({
             query: (id) => `videos/${id}`,
+            // We can not simply use invalidatesTags: ["Videos"] here. Because now we have to invalidate tags for a specific video. That is why we need dynamic tags for each video.
+            providesTags: (result, error, arg) => [{ type: "Video", id: arg }],
         }),
         // http://localhost:9000/videos?title_like=javascript&title_like=react&id_ne=6
         getRelatedVideos: builder.query({
             query: ({ id, title }) => {
                 const tags = title.split(" ");
                 const likes = tags.map((tag) => `title_like=${tag}`);
-                const queryString = `videos?${likes.join("&")}&_limit=4&id_ne=${id}`;
+                const queryString = `videos?${likes.join(
+                    "&"
+                )}&_limit=4&id_ne=${id}`;
                 return queryString;
             },
+            providesTags: (result, error, arg) => [
+                { type: "RelatedVideos", id: arg.id },
+            ],
         }),
         addVideo: builder.mutation({
             query: (data) => ({
@@ -44,14 +52,29 @@ export const apiSlice = createApi({
             invalidatesTags: ["Videos"],
         }),
         editVideo: builder.mutation({
-            query: ({id, data}) => ({
+            query: ({ id, data }) => ({
                 url: `videos/${id}`,
                 method: "PATCH",
                 body: data,
             }),
+            // We can not simply use invalidatesTags: ["Videos"] here. Because now we have to invalidate tags for a specific video. That is why we need dynamic tags for each video.
+            // result - After the mutation is done, we can get the result from the result property. We can use this result to update the cache. Also we can use any other property from the result object to make dynamic id for the cache.
+            // error - If there is an error, we can get the error from the error property.
+            // arg - We can get the argument we passed to the mutation from the arg property.
+            invalidatesTags: (result, error, arg) => [
+                "Videos",
+                { type: "Video", id: arg.id },
+                { type: "RelatedVideos", id: arg.id },
+            ],
         }),
-    })
+    }),
 });
 
 // so how do we get the queries, redux query will return us hooks. so we can use the hooks in our components. Here in our endpoints we declared a property with the name getVideos, that is why apiSlice returned us with a hook with the same name by adding a suffix and prefix. use + GetVideos + Query.
-export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery, useAddVideoMutation, useEditVideoMutation } = apiSlice;
+export const {
+    useGetVideosQuery,
+    useGetVideoQuery,
+    useGetRelatedVideosQuery,
+    useAddVideoMutation,
+    useEditVideoMutation,
+} = apiSlice;
