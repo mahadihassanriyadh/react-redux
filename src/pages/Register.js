2ff64949752c2fd21111ccfd34a0b33e1afb7621
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-13 14:32:23 +0600

    registration done

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,54 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import logoImage from "../assets/images/lws-logo-light.svg";
+import { useRegisterMutation } from "../features/auth/authApi";
+import Error from "../components/ui/Error";
 
 export default function Register() {
+    const navigate = useNavigate();
+    
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirrmPassword, setConfirmPassword] = useState("");
+    const [agreed, setAgreed] = useState(false);
+    const [error, setError] = useState("");
+
+    const [register, { data, isLoading, error: responseError }] =
+        useRegisterMutation();
+
+    const resetForm = () => {
+        setName("");
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+        setAgreed(false);
+        setError("");
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setError("");
+        if (password !== confirrmPassword) {
+            setError("Passwords do not match");
+            return;
+        } else {
+            register({ name, email, password });
+        }
+    };
+
+    useEffect(() => {
+        console.log(data);
+        console.log(responseError);
+        if (responseError?.data) {
+            setError(responseError.data);
+        } else if (data?.accessToken && data?.user) {
+            setError("");
+            resetForm();
+            navigate("/inbox");
+        }
+    }, [data, responseError, navigate]);
+
     return (
         <div className="grid place-items-center h-screen bg-[#F9FAFB">
             <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -18,8 +65,12 @@ export default function Register() {
                             Create your account
                         </h2>
                     </div>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
-                        <input type="hidden" name="remember" value="true" />
+                    <form
+                        className="mt-8 space-y-6"
+                        action="#"
+                        method="POST"
+                        onSubmit={handleSubmit}
+                    >
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
                                 <label htmlFor="name" className="sr-only">
@@ -33,6 +84,8 @@ export default function Register() {
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Name"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
                                 />
                             </div>
 
@@ -51,6 +104,8 @@ export default function Register() {
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Email address"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
                                 />
                             </div>
 
@@ -66,6 +121,10 @@ export default function Register() {
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Password"
+                                    value={password}
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
                                 />
                             </div>
 
@@ -84,6 +143,10 @@ export default function Register() {
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="confirmPassword"
+                                    value={confirrmPassword}
+                                    onChange={(e) =>
+                                        setConfirmPassword(e.target.value)
+                                    }
                                 />
                             </div>
                         </div>
@@ -91,10 +154,13 @@ export default function Register() {
                         <div className="flex items-center justify-between">
                             <div className="flex items-center">
                                 <input
-                                    id="remember-me"
-                                    name="remember-me"
+                                    id="agree"
+                                    name="agree"
                                     type="checkbox"
                                     className="h-4 w-4 text-violet-600 focus:ring-violet-500 border-gray-300 rounded"
+                                    required
+                                    checked={agreed}
+                                    onChange={() => setAgreed(!agreed)}
                                 />
                                 <label
                                     htmlFor="accept-terms"
@@ -109,11 +175,13 @@ export default function Register() {
                             <button
                                 type="submit"
                                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                                disabled={isLoading}
                             >
                                 <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
                                 Sign up
                             </button>
                         </div>
+                        {error && <Error message={error} />}
                     </form>
                 </div>
             </div>
