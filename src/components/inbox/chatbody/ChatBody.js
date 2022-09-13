daa94bb0fa0956df6a864d86aa55a2455a112d60
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-14 01:51:15 +0600

    module 10 done

--- src/components/inbox/chatbody/ChatBody.js
+++ src/components/inbox/chatbody/ChatBody.js
@@ -26,10 +26,7 @@ export default function ChatBody() {
     } else {
         content = (
             <>
-                <ChatHead
-                    avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
-                    name="Akash Ahmed"
-                />
+                <ChatHead message={messages[0]} />
                 <Messages messages={messages} />
                 <Options />
             </>
@@ -38,9 +35,7 @@ export default function ChatBody() {
 
     return (
         <div className="w-full lg:col-span-2 lg:block">
-            <div className="w-full grid conversation-row-grid">
-                {content}
-            </div>
+            <div className="w-full grid conversation-row-grid">{content}</div>
         </div>
     );
 }
