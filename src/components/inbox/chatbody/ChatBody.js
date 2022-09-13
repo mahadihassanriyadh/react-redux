53a7581750702e2a99d38a4f376e9aa714482c06
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-14 01:07:25 +0600

    will work on chat head now

--- src/components/inbox/chatbody/ChatBody.js
+++ src/components/inbox/chatbody/ChatBody.js
@@ -1,19 +1,45 @@
 // import Blank from "./Blank";
+import { useParams } from "react-router-dom";
+import { useGetMessagesQuery } from "../../../features/messages/messagesApi";
 import ChatHead from "./ChatHead";
 import Messages from "./Messages";
 import Options from "./Options";
+import Error from "../../ui/Error";
 
 export default function ChatBody() {
-    return (
-        <div className="w-full lg:col-span-2 lg:block">
-            <div className="w-full grid conversation-row-grid">
+    const { id } = useParams();
+    const {
+        data: messages,
+        isLoading,
+        isError,
+        error,
+    } = useGetMessagesQuery(id);
+
+    // decide what to render
+    let content = null;
+    if (isLoading) {
+        content = <div>Loading...</div>;
+    } else if (isError) {
+        content = <Error message={error?.data} />;
+    } else if (messages.length === 0) {
+        content = <div>No conversation</div>;
+    } else {
+        content = (
+            <>
                 <ChatHead
                     avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
                     name="Akash Ahmed"
                 />
-                <Messages />
+                <Messages messages={messages} />
                 <Options />
-                {/* <Blank /> */}
+            </>
+        );
+    }
+
+    return (
+        <div className="w-full lg:col-span-2 lg:block">
+            <div className="w-full grid conversation-row-grid">
+                {content}
             </div>
         </div>
     );
--- src/features/messages/messagesApi.js
+++ src/features/messages/messagesApi.js
@@ -1,7 +1,25 @@
 import { apiSlice } from "../api/apiSlice";
 
-export const msessagesApi = apiSlice.injectEndpoints({
+export const messagesApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
+        getMessages: builder.query({
+            query: (id) => `messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`,
+        }),
         
-    })
+    }),
 });
+
+export const { useGetMessagesQuery } = messagesApi;
+
+// import { apiSlice } from "../api/apiSlice";
+
+// export const messagesApi = apiSlice.injectEndpoints({
+//     endpoints: (builder) => ({
+//         getConversations: builder.query({
+//             query: (id) => `messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`,
+//         }),
+        
+//     }),
+// });
+
+// export const { useGetMessagesQuery } = messagesApi;
