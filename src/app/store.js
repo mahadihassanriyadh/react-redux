9a89d2ebc3907a3b420eccb4ee605371e9e0a7bf
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-13 09:42:42 +0600

    api slice and code spliting and configuring store done

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { apiSlice } from '../features/api/apiSlice';
+import authSliceReducer from '../features/auth/authSlice';
+import messagesSliceReducer from '../features/messages/messagesSlice';
+import conversationsSliceReducer from '../features/conversations/conversationsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSliceReducer,
+    messages: messagesSliceReducer,
+    conversations: conversationsSliceReducer,
   },
+  // এখানে usually redux devTools আমাদের appplication এ always ই on থাকছে। কিন্তু তা একটা problem, কারণ এতে করে যেকেও redux devtool use করে আমাদের store এর data গুলো দেখতে পাবে। যা production build এর জন্য উপযোগী নয়। তাই আমরা চাইলে এই devtool কে production build এ disable করতে পারি। এটা করার জন্য আমরা এই লাইনটা আমাদের configureStore এর ভিতরে যোগ করব। এখানে process.env.NODE_ENV এর মান যদি development হয় তাহলে devtool কে enable করবে। কিন্তু যদি production হয় তাহলে devtool কে disable করবে। process.env.NODE_ENV হচ্ছে REACT APP এর environment check করার একটা way। 
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
 });
---
+++ src/features/messages/messagesSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+};
+
+const messageSlice = createSlice({
+    name: "messages",
+    initialState,
+    reducers: {
+
+    },
+});
+
+export const { } = messageSlice.actions;
+export default messageSlice.reducer;
