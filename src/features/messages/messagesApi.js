c6c85ccdbcb0657b31025014f0234cd004a6df66
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-09-15 12:28:06 +0600

    ....

--- src/features/messages/messagesApi.js
+++ src/features/messages/messagesApi.js
@@ -5,11 +5,18 @@ export const messagesApi = apiSlice.injectEndpoints({
         getMessages: builder.query({
             query: (id) => `messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`,
         }),
+        addMessage: builder.mutation({
+            query: (data) => ({
+                url: "messages",
+                method: "POST",
+                body: data,
+            }),
+        }),
         
     }),
 });
 
-export const { useGetMessagesQuery } = messagesApi;
+export const { useGetMessagesQuery, useAddMessageMutation } = messagesApi;
 
 // import { apiSlice } from "../api/apiSlice";
 
